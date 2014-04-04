ad99af87aa2780bf862f7f4d3caaa6d47d6e3774
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-04-04 15:43:51 -0600

    ATSrails_added "main" to the main controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 App::Application.routes.draw do
 
+get "/"                              => "main#index"
+get "/main/about"                    => "main#about"
+get "/main/contact"                  => "main#contact"
+get "/main/:product_category"        => "main#product_category"
+get "/main/more_info/:id"            => "main#more_info_id"
+
+get "/user"           => "user#index",                  as: "sign_in"
+post "/user/sign_in"  => "user#sign_in",                as: "logged_in"
+get "/user/new"       => "user#new"
+post "/user"          => "user#create"
+get "/user/:id/edit"  => "user#edit"
+put "/user/:id"       => "user#update"
+get "/user/logout"    => "user#logout"
+
 get "/admin"                  => "admin#index",                     as: "admin_signin"
 get "/admin/links"            => "admin#links",                     as: "links"
 get "/admin/new"              => "admin#new",                       as: "new_product"
@@ -10,21 +24,5 @@ get "/admin/product_sale"     => "admin#product_sale",              as: "/produc
 post "/admin/product_sale/"   => "admin#product_sale_edit_post",    as: "/product_sale1" 
 get "/admin/category_sale/"   => "admin#category_sale_edit",        as: "/category_sale"
 post "/admin/category_sale/"  => "admin#category_sale_edit_post",   as: "/category_sale1"  
-
-
-get "/user"           => "user#index",                 as: "sign_in"
-post "/user/sign_in"  => "user#sign_in",                as: "logged_in"
-get "/user/new"       => "user#new"
-post "/user"          => "user#create"
-get "/user/:id/edit"  => "user#edit"
-put "/user/:id"       => "user#update"
-get "/user/logout"    => "user#logout"
-
-
-get "/"                   => "main#index"
-get "/about"              => "main#about"
-get "/contact"            => "main#contact"
-get "/:product_category"  => "main#product_category"
-get "/more_info/:id"      => "main#more_info_id"
 
 end
