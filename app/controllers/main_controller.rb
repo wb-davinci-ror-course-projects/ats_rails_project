5eaf4baa4570ecaec698926c2cff632d6ad29ed6
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-04-04 15:51:12 -0600

    ATSrails-minor changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,32 +1,29 @@
 class MainController < ApplicationController
 
-def index
-  render :index and return
-end
+  def index
+    render :index and return
+  end
 
-def product_category
-  product_category = params[:product_category]
-  @header = "#{product_category}"
-  @products = Product.where(category: product_category)
-  render :show and return
-end
+  def product_category
+    product_category = params[:product_category]
+    @header = "#{product_category}"
+    @products = Product.where(category: product_category)
+    render :show and return
+  end
 
-def more_info_id
-  id = params[:id]
-  @header = "Product Detail Page"
-  @product = Product.find_by(id: id)
-  render :more_info and return
-end
+  def more_info_id
+    id = params[:id]
+    @header = "Product Detail Page"
+    @product = Product.find_by(id: id)
+    render :more_info and return
+  end
 
-def about
-  render :about and return
-end
+  def about
+    render :about and return
+  end
 
-def contact
-  render :contact and return
-end
-
-
-
+  def contact
+    render :contact and return
+  end
 
 end
