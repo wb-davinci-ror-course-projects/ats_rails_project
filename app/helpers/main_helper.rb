957a13eb6a8836b8ba6a4cd1fe78fd0d2c8bba35
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-04-20 09:53:42 -0600

    ATSrails_added helper add_to_cart to show view

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -8,11 +8,25 @@ def flash_alert (x)
   end
 end
 
-def out_of_stock(q, p)
-  if q > 0 
-    add_to_cart(i)
-  else 
-    "This item is currently out of stock.".html_safe
+def add_to_cart (p)
+  if p.quantity > 0
+    "<h5 class='pad'>#{button_tag 'Add to cart', name: 'product_id', value: p.id}</h5>".html_safe
+  else
+    "<h4 class='pad'>'This item is currently out of stock.'</h4>".html_safe
   end
 end
+
+def image_display (p)
+  if Image.find_by(name: p.product_code) == nil
+      "<a class='a:hover' href='/images/#{p.image}.jpg/'>
+        <img src='/images/#{p.image}.jpg/' width='180' height='135'></img>
+      </a>".html_safe
+  else
+    image = Image.find_by(name: p.product_code)
+      "<a class='a:hover' href='/images/#{p.image}.jpg/'>
+        <img src='#{image_data_path(image.id, image.extension)}' width='180' height='135'>
+       </a>".html_safe
+  end 
+end
+
 end
