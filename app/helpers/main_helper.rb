2b99241db7cfa9f7494737a6bc8031203f62a490
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-04-21 17:49:47 -0600

    ATSrails_show styling changes

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -10,7 +10,7 @@ end
 
 def add_to_cart (p)
   if p.quantity > 0
-    "<h5 color: black;'>#{label_tag :quantity}
+    "<h5 class='pad'>#{label_tag :quantity}
     #{text_field_tag "quantity_#{p.id}", 1, size: 1}
     #{button_tag 'Add to cart', name: 'product_id', value: p.id}</h5>".html_safe
   else
@@ -22,12 +22,12 @@ end
 def image_display (p)
   if Image.find_by(name: p.product_code) == nil
       "<a class='a:hover' href='/images/#{p.image}.jpg/'>
-        <img src='/images/#{p.image}.jpg/' width='180' height='135'></img>
+        <img src='/images/#{p.image}.jpg/' width='250' height='187'></img>
       </a>".html_safe
   else
     image = Image.find_by(name: p.product_code)
       "<a class='a:hover' href='/images/#{p.image}.jpg/'>
-        <img src='#{image_data_path(image.id, image.extension)}' width='180' height='135'>
+        <img src='#{image_data_path(image.id, image.extension)}' width='250' height='187'>
        </a>".html_safe
   end 
 end
